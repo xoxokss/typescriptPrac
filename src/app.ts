665468e58eede93f194979c2ce8a059120faab03
xoxokss <104071351+xoxokss@users.noreply.github.com>
2024-03-30 22:33:38 +0900

    Feat : Learn to Class getter setter.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class Department {
     // private name: string;
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor(public name: string, public readonly id: string) {}
 
@@ -43,11 +43,37 @@ blockchain.describe();
 console.log(blockchain);
 
 class AccountingDepartment extends Department {
+    private lastReport : string;
+
+    get mostRecentReport () {
+        if(this.lastReport){
+            return this.lastReport;
+        }
+        throw new Error("No report found.")
+    }
+
+    set mostRecentReport (value) {
+        if (!value){
+            throw new Error("Please pass in valid value.")
+        }
+        this.addReport(value)
+    }
+
     constructor(id: string, private reports: string[]) {
         super('Accounting', id);
+        this.lastReport = reports[0];
     }
+    addEmployee(employeeName: string) {
+        console.log("input name : ", employeeName)
+        if (employeeName == 'Max') {
+            return;
+        }
+        this.employees.push(employeeName);
+    }
+
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReport() {
         console.log(this.reports);
@@ -56,13 +82,16 @@ class AccountingDepartment extends Department {
 
 const accounting = new AccountingDepartment('d1', []);
 
-accounting.addReport('years');
-accounting.addReport('secret')
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-// // accounting.employees[2] = 'frydman';
+accounting.addReport('Year Start Report');
+
+console.log(accounting.mostRecentReport);
+
+accounting.mostRecentReport = 'Year End Report';
+accounting.addReport('secret');
 accounting.name = 'Accounting2';
 accounting.addEmployee('frydman');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.printEmpployeeInformation();
 accounting.describe();
 
